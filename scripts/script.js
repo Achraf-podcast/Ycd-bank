dd71369cfe557e4dc5534284863b07954d323a26
Achraf-podcast <achrafagourram6@gmail.com>
2025-11-10 16:05:32 +0100

    Recreate how tables appear and starting on javascript

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const transactions = [
+    {id: 1, date: "2025-10-26", type: "", amount: 100, amountType: true},
+    {id: 2, date: "2025-10-26", type: "", amount: 90, amountType: true},
+    {id: 3, date: "2025-10-26", type: "", amount: 800, amountType: true},
+    {id: 4, date: "2025-10-26", type: "", amount: 35, amountType: true}
+]
