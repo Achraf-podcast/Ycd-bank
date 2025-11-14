2cca7720da546397d65796f7d50df242c04a47bf
Achraf-podcast <achrafagourram6@gmail.com>
2025-11-14 15:08:52 +0100

    Fixing the localstorage bug

--- scripts/script.js
+++ scripts/script.js
@@ -998,59 +998,30 @@ login_form.addEventListener("submit", (event) => {
         display_red_notification("Identifier or password incorrect");
     }
 })
-const transactions = [
-    { id: 1, date: "2025-10-26", type: "Payment", amount: 100, amountType: true },
-    { id: 2, date: "2025-10-26", type: "Transfer", amount: -90, amountType: false },
-    { id: 3, date: "2025-10-26", type: "Transfer", amount: 800, amountType: true },
-    { id: 4, date: "2025-10-26", type: "Recharge", amount: -35, amountType: false },
 
-    { id: 5, date: "2025-10-26", type: "Payment", amount: 200, amountType: true },
-    { id: 6, date: "2025-10-26", type: "Transfer", amount: -20, amountType: false },
-    { id: 7, date: "2025-10-26", type: "Transfer", amount: 10, amountType: true },
-    { id: 8, date: "2025-10-26", type: "Recharge", amount: -5, amountType: false },
-
-    { id: 9, date: "2025-10-26", type: "Transfer", amount: 800, amountType: true },
-    { id: 10, date: "2025-10-26", type: "Recharge", amount: -35, amountType: false },
-    { id: 11, date: "2025-10-26", type: "Payment", amount: 100, amountType: true },
-    { id: 12, date: "2025-10-26", type: "Transfer", amount: -90, amountType: false },
-
-    { id: 13, date: "2025-10-26", type: "Transfer", amount: 800, amountType: true },
-    { id: 14, date: "2025-10-26", type: "Recharge", amount: -35, amountType: false },
-    { id: 15, date: "2025-10-26", type: "Transfer", amount: 800, amountType: true },
-    { id: 16, date: "2025-10-26", type: "Recharge", amount: -35, amountType: false },
-
-    { id: 17, date: "2025-10-26", type: "Transfer", amount: 800, amountType: true },
-    { id: 18, date: "2025-10-26", type: "Recharge", amount: -35, amountType: false },
-    { id: 19, date: "2025-10-26", type: "Transfer", amount: 800, amountType: true },
-    { id: 20, date: "2025-10-26", type: "Recharge", amount: -35, amountType: false },
-
-    { id: 21, date: "2025-10-26", type: "Transfer", amount: 800, amountType: true },
-    { id: 22, date: "2025-10-26", type: "Recharge", amount: -35, amountType: false },
-    { id: 23, date: "2025-10-26", type: "Recharge", amount: -35, amountType: false }
-];
-function showTransactionTable(id, date, type, amount, amountType) {
+function showTransactionTable(id, date, type, amount, amountType){
     const original = document.querySelector('.transaction-table');
     const container = document.getElementById('transactions-container');
     const newTable = original.cloneNode(true);
 
     newTable.classList.remove('hidden');
-    newTable.classList.add("id" + id);
+    newTable.classList.add("id"+id);
     const spans = newTable.querySelectorAll('span');
     spans[1].textContent = date;
     spans[3].textContent = type;
-    spans[4].textContent = amount + '$';
+    spans[4].textContent = amount+'$';
     spans[4].style.color = amountType ? "#5CFF98" : "#EB5757";
 
     container.appendChild(newTable);
 }
-function separateTransactions(arr) {
+function separateTransactions(arr){
     const updatedtransactions = [];
     let temp = [];
     let counter = 0;
     let index = 1;
     arr.forEach(transaction => {
-        temp[index - 1] = transaction;
-        if (index % 4 === 0) {
+        temp[index-1] = transaction;
+        if(index%4 === 0){
             updatedtransactions[counter] = temp;
             temp = [];
             counter++;
@@ -1059,95 +1030,81 @@ function separateTransactions(arr) {
     });
 
     let rest = arr.length % 4;
-    if (rest) {
-        for (let i = 0; i < rest; i++) {
+    if(rest){
+        for(let i=0; i<rest; i++){
             updatedtransactions[counter] = temp.filter(obj => Object.keys(obj).length > 0);
         }
     }
 
-    for (let i = 0; i < updatedtransactions.length; i++) {
+    for(let i=0; i<updatedtransactions.length; i++){
         updatedtransactions[i] = updatedtransactions[i].filter(obj => Object.keys(obj).length > 0);
     }
 
     return updatedtransactions
 }
-function showTransactionOfEachTable() {
-    for (let i = 0; i < paginationBtns.length; i++) {
-        if (paginationBtns[i].classList.contains('bg-[#283039]')) {
-            allTransactions[parseInt(paginationBtns[i].textContent) - 1].forEach(t => {
+function showTransactionOfEachTable(){
+    for(let i=0; i<paginationBtns.length; i++){
+        if(paginationBtns[i].classList.contains('bg-[#283039]')){
+            allTransactions[parseInt(paginationBtns[i].textContent)-1].forEach(t => {
                 showTransactionTable(t.id, t.date, t.type, t.amount, t.amountType);
             });
         }
     }
 }
-function switchPagination(num) {
-    for (let i = 0; i < 4; i++) {
-        try { paginationBtns[i].classList.remove('bg-[#283039]'); } catch { }
-        paginationBtns[num % 4].classList.add('bg-[#283039]');
+function switchPagination(num){
+    for(let i=0; i<4; i++){
+        try{paginationBtns[i].classList.remove('bg-[#283039]');}catch{}
+        paginationBtns[num%4].classList.add('bg-[#283039]');
     }
 }
-function showPaginationButtons() {
-    for (let i = paginationIndex; i <= paginationIndex + 3; i++) {
-        if (i <= allTransactions.length) {
+function showPaginationButtons(){
+    for(let i=paginationIndex; i<=paginationIndex+3; i++){
+        if(i<=allTransactions.length){
             let btn = document.createElement("button");
             btn.classList = "text-sm font-normal leading-normal flex size-10 items-center justify-center text-white rounded-full pagination";
             btn.textContent = i;
             document.getElementById('pagination-container').insertBefore(btn, document.getElementById('next-arrow'));
-        } else { break }
+        }else{break}
     }
-
-    try {
-        if (parseInt(paginationBtns[actualIndex - 1].textContent) === actualIndex) { paginationBtns[actualIndex - 1].classList.add("bg-[#283039]"); }
-    } catch { }
+    
+    try{
+        if(parseInt(paginationBtns[actualIndex-1].textContent) === actualIndex){paginationBtns[actualIndex-1].classList.add("bg-[#283039]");}
+    }catch{}
 }
 
 document.getElementById("pagination-container").addEventListener("click", (e) => {
-    if (e.target.textContent != "") {
-        try {
-            if (!Number.isNaN(parseInt(e.target.textContent))) { actualIndex = parseInt(e.target.textContent); }
-            switchPagination(parseInt(e.target.textContent) - 1);
+    if(e.target.textContent != ""){
+        try{
+            if(!Number.isNaN(parseInt(e.target.textContent))){actualIndex = parseInt(e.target.textContent);}
+            switchPagination(parseInt(e.target.textContent)-1);
             document.getElementById('transactions-container').innerHTML = "";
             showTransactionOfEachTable();
-        } catch { }
+        }catch{}
     }
-    else {
-        try {
-            if (e.target.alt == "arrow right icon") {
-                if (allTransactions.length > paginationIndex + 4) { paginationIndex += 4; }
-            } else {
-                if (paginationIndex > 1) { paginationIndex -= 4; }
+    else{
+        try{
+            if(e.target.alt == "arrow right icon"){
+                if(allTransactions.length>paginationIndex+4){paginationIndex += 4;}
+            }else{
+                if(paginationIndex>1){paginationIndex -= 4;}
             }
             document.querySelectorAll(".pagination").forEach(btn => btn.remove());
             showPaginationButtons();
-        } catch { }
+        }catch{}
     }
 });
 
-
-var allTransactions = separateTransactions(transactions);
-var actualIndex = 1;
-var paginationIndex = 1;
-showPaginationButtons();
-var paginationBtns = document.getElementsByClassName("pagination");
-paginationBtns[actualIndex - 1].classList.add("bg-[#283039]");
-
-showTransactionOfEachTable();
-async function convertCurrency(amount, from, to) {
-    document.querySelector('#converted-currency').textContent = "Loading...";
-    try {
-        const response = await fetch(`${API_URL}${from}`);
-        const data = await response.json();
-        const rate = data.conversion_rates[to];
-        const convertedAmount = amount * rate;
-
-        document.querySelector('#converted-currency').textContent = convertedAmount.toFixed(2);
-    } catch { document.querySelector('#converted-currency').textContent = "An error caused by the server, please try again"; }
+try{
+    if(JSON.parse(localStorage.getItem("users"))[0].transactions.length != 0){
+    var allTransactions = separateTransactions(JSON.parse(localStorage.getItem("users"))[0].transactions);
+    var actualIndex = 1;
+    var paginationIndex = 1;
+    document.getElementById("pagination-container").classList.remove('hidden');
+    showPaginationButtons();
+    var paginationBtns = document.getElementsByClassName("pagination");
+    paginationBtns[actualIndex-1].classList.add("bg-[#283039]");
+    showTransactionOfEachTable();
+}else{document.getElementById("no_transactions_message2").classList.remove('hidden')}
+}catch{
+    document.getElementById("no_transactions_message2").classList.remove('hidden')
 }
-
-let inputAmount = document.getElementsByClassName('convert-input')[0];
-let inputFrom = document.getElementsByClassName('convert-input')[1];
-let inputTo = document.getElementsByClassName('convert-input')[2];
-
-const API_URL = "https://v6.exchangerate-api.com/v6/a52906b05cf0547eb05bfe81/latest/";
-
-document.getElementById('convert-currency-btn').addEventListener('click', () => convertCurrency(inputAmount.value, inputFrom.value, inputTo.value))
